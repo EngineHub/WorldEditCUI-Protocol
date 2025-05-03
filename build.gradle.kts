01ac4a62b2367352d44fe589a802e7fcfcced9ae
Maddy Miller <git@madelinemiller.dev>
2025-05-03 14:36:40 +1000

    Add artifactory publishing tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
 import net.kyori.indra.licenser.spotless.IndraSpotlessLicenserExtension
+import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
 
 plugins {
     base
     alias(libs.plugins.loom) apply false
     alias(libs.plugins.spotless)
     alias(libs.plugins.indra.spotlessLicenser) apply false
+    id("com.jfrog.artifactory") version "5.2.5"
 }
 
 allprojects {
@@ -29,6 +32,7 @@ subprojects {
     apply(plugin = "com.diffplug.spotless")
     apply(plugin = "net.kyori.indra.licenser.spotless")
     apply(plugin = "maven-publish")
+    apply(plugin = "com.jfrog.artifactory")
 
     val targetJavaVersion: String by project
     val targetVersion = targetJavaVersion.toInt()
@@ -79,6 +83,10 @@ subprojects {
             tasks.matching { it.name == "genSources" }.configureEach {
                 setDependsOn(setOf("genSourcesWithVineflower"))
             }
+
+            tasks.named<ArtifactoryTask>("artifactoryPublish") {
+                publications("maven")
+            }
         }
 
         dependencies {
@@ -105,4 +113,30 @@ subprojects {
             }
         }
     }
+}
+
+val ARTIFACTORY_CONTEXT_URL = "artifactory_contextUrl"
+val ARTIFACTORY_USER = "artifactory_user"
+val ARTIFACTORY_PASSWORD = "artifactory_password"
+
+if (!project.hasProperty(ARTIFACTORY_CONTEXT_URL)) ext[ARTIFACTORY_CONTEXT_URL] = "http://localhost"
+if (!project.hasProperty(ARTIFACTORY_USER)) ext[ARTIFACTORY_USER] = "guest"
+if (!project.hasProperty(ARTIFACTORY_PASSWORD)) ext[ARTIFACTORY_PASSWORD] = ""
+
+configure<ArtifactoryPluginConvention> {
+    setContextUrl("${project.property(ARTIFACTORY_CONTEXT_URL)}")
+    clientConfig.publisher.run {
+        repoKey = when {
+            "${project.version}".contains("SNAPSHOT") -> "libs-snapshot-local"
+            else -> "libs-release-local"
+        }
+        username = "${project.property(ARTIFACTORY_USER)}"
+        password = "${project.property(ARTIFACTORY_PASSWORD)}"
+        isMaven = true
+        isIvy = false
+    }
+}
+
+tasks.named<ArtifactoryTask>("artifactoryPublish") {
+    isSkip = true
 }
